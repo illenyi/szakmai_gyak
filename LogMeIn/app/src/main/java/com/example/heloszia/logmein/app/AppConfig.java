34abc57f8dac8d862d686f3575365d4a7cbefc6f
Norbert Illényi <illenyinorbert@gmail.com>
2016-08-08 14:13:28 +0200

    init
    lul

---
+++ LogMeIn/app/src/main/java/com/example/heloszia/logmein/app/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.heloszia.logmein.app;
+
+/**
+ * Created by heloszia on 2016. 08. 04..
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://10.244.0.100//android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://10.244.0.100//android_login_api/register.php";
+}
